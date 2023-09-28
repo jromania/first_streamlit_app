26bd8a305a11acf0b18542f7cf038bdf7e9a5053
jromania <126221332+jromania@users.noreply.github.com>
2023-09-28 10:54:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 # 🥣 🥗 🐔 🥑
 streamlit.title("My Parents New Healhty Diner")
 streamlit.header("Breakfast Menu")
